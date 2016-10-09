3fca41d911717615e4f9a6bae719f001d4a367f5
Sang Tran <sangtran.code@gmail.com>
2016-10-09 02:55:58 -0500

    Update app.js

--- homeworks/lecture03/EventEmitter-exercises/app.js
+++ homeworks/lecture03/EventEmitter-exercises/app.js
@@ -1,1 +1,23 @@
 
+/*********************************************** 
+	CS572 - MODERN WEB APPLICATION PROGRAMMING
+	Maharishi University of Management
+	Department of Computer Science 
+	Professor: ASAAD SAAD
+	Student: SANG TRAN (#985259)
+	Lecture 3 / Event Emitter exercise 2
+************************************************/
+
+/* Exercise 2 - Build a script that instantiates one Ticker and bind to the "tick" event, 
+printing "TICK" every time it gets one. */
+
+// create a Ticker instance object
+var Ticker = require('./Ticker');
+var ticker = new Ticker();
+
+// handle on 'tick' event
+ticker.on('tick', function(){
+  console.log('TICK');
+});
+
+ticker.startTicker();
