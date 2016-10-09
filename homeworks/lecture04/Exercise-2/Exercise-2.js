520a7589af52e68d980c53b65e7b24edd5e7e497
Sang Tran <sangtran.code@gmail.com>
2016-10-09 15:37:19 -0500

    Update Exercise-2.js

--- homeworks/lecture04/Exercise-2/Exercise-2.js
+++ homeworks/lecture04/Exercise-2/Exercise-2.js
@@ -4,20 +4,10 @@
   Department of Computer Science 
   Professor: ASAAD SAAD
   Student: SANG TRAN (#985259)
-  Lecture 3 / Image serves exercise / 
-        'image bigger than 3MB'
+  Lecture 4 / Exercise 2
 ************************************************/
 
-/* Exercise - Create a web server that's going to send a big image (bigger then 3MB)
-to any client that send a request to your specified server:port. Use the best way for performance.*/
+/* Exercise - Make an HTTP server that saves the request body into a file. */
 
 // using these libraries
 var fs = require('fs');
-var http = require('http');
-
-// create a server for image serving
-http.createServer(function(req, res){
-  var img = fs.readFileSync(__dirname + '/big-image.png');
-  res.writeHead(200, {'Content-Type': 'image/png' });
-  res.end(img, 'binary');
-}).listen(8080);
