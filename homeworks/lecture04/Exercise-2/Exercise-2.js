d6b860351c9bfc673dd05fdcc0e4181143e637c0
Sang Tran <sangtran.code@gmail.com>
2016-10-09 17:15:24 -0500

    Update Exercise-2.js

--- homeworks/lecture04/Exercise-2/Exercise-2.js
+++ homeworks/lecture04/Exercise-2/Exercise-2.js
@@ -9,5 +9,26 @@
 
 /* Exercise - Make an HTTP server that saves the request body into a file. */
 
-// using these libraries
+// To test: http://localhost:8080/start?foo=bar&hello=world
+const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 var fs = require('fs');
+
+function onRequest(request, response){
+
+  // reading request data
+  const urlStr = request.url;
+  const pathName = url.parse(urlStr).pathname;
+  const query = url.parse(urlStr).query;
+  const jsonRequestData = JSON.stringify(querystring.parse(query));
+
+  // write to file
+  fs.writeFile('request-body.json', jsonRequestData);
+
+  // end response
+  response.end('Please check the file: "' + __dirname + '/request-body.json"');
+}
+
+// actived server on port 8080 with the onRequest as a callback function
+http.createServer(onRequest).listen('8080');
